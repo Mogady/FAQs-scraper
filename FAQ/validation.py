48728d06c1911e0b324882ebe1b37e0cbe3225ad
Ahmed Magdy <ahmedmagdyno@gmail.com>
2021-04-10 22:04:02 -0500

    Update validation.py

--- FAQ/validation.py
+++ FAQ/validation.py
@@ -24,7 +24,7 @@ def validate_doc(document, company, logger):
 
                   }
               },
-              "required": ["answer", "question", "company", "category", "html_url"],
+              "required": ["answer", "question", "company", "category", "url"],
               "definitions": {
                   "non-empty-string": {
                       "type": "string",
